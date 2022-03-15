4f798eecb0aa73d408122e283a39c82a3a7d6647
r <meetrrahul0@gmail.com>
2022-03-15 15:39:16 +0530

    update

--- TextAnalyzer/File.cs
+++ TextAnalyzer/File.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 
-namespace TestReading
+namespace TextAnalyzer
 {
     public class File
     {
--- TextAnalyzer/FileOption.cs
+++ TextAnalyzer/FileOption.cs
@@ -3,7 +3,7 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace TestReading
+namespace TextAnalyzer
 {
     public class FileOption
     {
